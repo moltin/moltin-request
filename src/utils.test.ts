3b5a48ffa65cc337b61c9a09ce7e90a7694f3ba2
Jamie Barton <hi@jamiebarton.co.uk>
2018-11-27 13:11:21 +0000

    add test for createCartIdentifier

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,7 +1,14 @@
-import { removeLeadingSlash } from './utils'
+import { removeLeadingSlash, createCartIdentifier } from './utils'
 
 it('leading slash is removed', () => {
   const string = '/test'
 
   expect(removeLeadingSlash(string)).toEqual('test')
+})
+
+it('unique cart identifier is created', () => {
+  const id = createCartIdentifier()
+  const id2 = createCartIdentifier()
+
+  expect(id).not.toEqual(id2)
 })
