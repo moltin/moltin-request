99216154118e3b4ed9f46e0da14f8460c3c5eead
Jamie Barton <hi@jamiebarton.co.uk>
2018-11-27 13:15:47 +0000

    add return type for createId func

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@ export function removeLeadingSlash(string: string) {
   return string.replace(/^\/+/, '')
 }
 
-export function createCartIdentifier() {
+export function createCartIdentifier(): string {
   return 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'.replace(/[x]/g, () =>
     ((Math.random() * 16) | 0).toString(16)
   )
