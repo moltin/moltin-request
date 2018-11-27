f81b86855ee3a4afdbf8b8e6ad735d6e58cfca20
Jamie Barton <hi@jamiebarton.co.uk>
2018-11-27 13:23:25 +0000

    Feat: Create cart identifier helper (#57)
    * create cart identifier func
    
    * add test for createCartIdentifier
    
    * add test for createCartIdentifier
    
    * add return type for createId func

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,7 +1,15 @@
 import { removeLeadingSlash } from './utils'
+import { createCartIdentifier } from './'
 
 it('leading slash is removed', () => {
   const string = '/test'
 
   expect(removeLeadingSlash(string)).toEqual('test')
+})
+
+it('unique cart identifier is created', () => {
+  const id = createCartIdentifier()
+  const id2 = createCartIdentifier()
+
+  expect(id).not.toEqual(id2)
 })
--- src/utils.ts
+++ src/utils.ts
@@ -1,3 +1,9 @@
 export function removeLeadingSlash(string: string) {
   return string.replace(/^\/+/, '')
 }
+
+export function createCartIdentifier(): string {
+  return 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'.replace(/[x]/g, () =>
+    ((Math.random() * 16) | 0).toString(16)
+  )
+}
